9faba34a9085ef72e342ddc1c900c1def5424478
Matthew Elmquist <mattelmquist@gmail.com>
2017-10-23 21:12:34 -0400

    file-loader and scc-loader added

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const paths = {
     DIST: path.resolve(__dirname, 'dist'),
@@ -17,6 +18,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: path.join(paths.SRC, 'index.html'),
         }),
+        new ExtractTextPlugin('style.bundle.css'),
     ],
     module: {
         rules: [
@@ -25,6 +27,18 @@ module.exports = {
                 exclude: /node_modules/,
                 use: [
                     'babel-loader',
+                ],
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract({
+                    use: 'css-loader',
+                }),
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    'file-loader',
                 ],
             },
         ],
